f0ce4abe5be848274278885157074855bd106feb
NimSreytouch <nimsreytouch@gmail.com>
2018-10-23 23:21:44 +0700

    kratie

---
+++ js/link.js
@@ -0,0 +1,13 @@
+var item1 =	'<div class="thumbnail">'
+		        + '<a href="1.hmtl" target="_blank">'
+		            + '<img src="../img/Bailinimg/ទឹកជ្រោះប្រាំពីរជាន់.png" alt="Fjords" style="width:100%">'
+		            + '<div class="caption">'
+		                + '<span>អូរអែម ជា​តំបន់​រមណីយដ្ឋាន​ចាស់​ ដែល​ស្ថិត​នៅ​ក្នុង​សង្កាត់​អូរ​ច្រារ ខ័ណ្ឌ​ប៉ៃលិន​មាន​ចំងាយ​១០គម​ពីក្រុង​ប៉ៃលិន​ហើយ​រមណីយដ្ឋាន​នេះ​ជារមណីយដ្ឋាន​ធម្មជាតិ។</span><span>......</span>'
+		            + '</div>'
+		        + '</a>'
+		    + '</div>';
+document.getElementById("item1").innerHTML = item1;
+
+var link =[
+	"1.html","2"
+]
